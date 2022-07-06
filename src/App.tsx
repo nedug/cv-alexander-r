a254905f7eb60a9cb0d674e958a776ba4add48a3
Alexander R <ru5nedug@mail.ru>
2022-07-06 15:42:27 +0300

    ff

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import Header from './header/Header';
 import Main from './main/Main';
 import AllSkills from './skills/AllSkills';
 import Contacts from './contacts/Contacts';
 import AllProjects from './projects/AllProjects';
-import Footer from './footer/Footer';
-import AboutMe from './hireMe/HireMe';
 
 export const App = () => (
     <div className="App">
