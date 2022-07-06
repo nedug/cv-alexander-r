ff4c4a3db8f07f7b0266a40c436544a437f8be98
Alexander R <ru5nedug@mail.ru>
2022-07-06 11:41:40 +0300

    refac

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import Header from "./header/Header";
 import Main from "./main/Main";
 import AllSkills from "./skills/AllSkills";
@@ -8,20 +7,14 @@ import AllProjects from "./projects/AllProjects";
 import Footer from "./footer/Footer";
 import AboutMe from "./hireMe/HireMe";
 
-function App() {
-    return (
-        <div className="App">
-            <Header/>
-            <Main/>
-            <AllSkills/>
-            <AllProjects/>
-            <AboutMe/>
-            <Contacts/>
-            <Footer/>
-        </div>
-    );
-}
- //git remote set-url origin https://github.com/katesunday/cv-ekaterina-kopylova.git
- //git remote set-url origin git@github.com:katesunday/cv-ekaterina-kopylova.git
-
-export default App;
+export const App = () => (
+    <div className="App">
+        <Header />
+        <Main />
+        <AllSkills />
+        <AllProjects />
+        <AboutMe />
+        <Contacts />
+        <Footer />
+    </div>
+);
