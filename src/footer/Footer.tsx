a22a23d54a9dd4b784e79725b472715172dd4a03
Alexander R <ru5nedug@mail.ru>
2022-07-11 08:35:13 +0300

    Update Footer.tsx

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -11,12 +11,10 @@ const Footer = () => {
                     <img className={s.linkedinLogo} src={''} alt="linkedin"/>
                 </a>
 
-
             </div>
             <div>
                 <p className={s.copyright}>© 2022 it-incubator.by All rights reserved.</p>
             </div>
-
         </div>
     );
 };
